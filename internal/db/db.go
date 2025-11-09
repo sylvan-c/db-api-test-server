1a4449b0ecc824832cbff5df46fd0a0195751ce5
sylvan <sylvanchouhan@gmail.com>
2025-11-09 16:09:57 +0000

    Add retries to db connection

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,18 +2,33 @@ package db
 
 import (
 	"database/sql"
+	"log"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
 func Connect(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		return nil, err
+	var db *sql.DB
+	var err error
+
+	for i := 0; i < 10; i++ {
+		db, err = sql.Open("postgres", dsn)
+		if err != nil {
+			log.Printf("DB connection failed: %v", err)
+		} else {
+			err = db.Ping()
+			if err == nil {
+				log.Println("Connected to DB")
+				return db, nil
+			}
+			log.Printf("DB ping failed: %v", err)
+		}
+
+		log.Println("Retrying in 2s...")
+		time.Sleep(2 * time.Second)
 	}
-	// Test connection
-	if err := db.Ping(); err != nil {
-		return nil, err
-	}
-	return db, nil
+
+	log.Fatalf("Could not connect to DB after retries: %v", err)
+	return nil, err
 }
